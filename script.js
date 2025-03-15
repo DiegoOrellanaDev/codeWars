a459e89d6a6d251f19187185b174ca86af69e99b
Diego <iroh.doragon@gmail.com>
2025-03-15 04:41:00 -0600

    daily exercise

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+//Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {  
+  let arr = numbers.sort((a,b) => a - b)
+  return arr[0] + arr[1]
+ }
 
+ 
 /*
 Jaden Casing Strings
 
