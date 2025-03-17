14b021456e300419a41b862f95894b28c2cb73f5
Diego <iroh.doragon@gmail.com>
2025-03-17 04:02:17 -0600

    daily exercise

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function sumTwoSmallestNumbers(numbers) {
   return arr[0] + arr[1]
  }
 
- 
+
 /*
 Jaden Casing Strings
 
@@ -29,3 +29,42 @@ Object.defineProperty(
 function reverseWords(str) {
   return str.split(' ').map(element => element.split('').reverse().join('')).join(' ')
 }
+
+
+let arrA = [1, -2, 7, 2, 1, 3, 7, 1, 0, 2, 3];
+let arrB = [2, -1, 1, 1, 1, 1, 2, 3, 3, 7, 7, 0];
+let rng = [-4, 4];
+let wanted = 'odd';
+function findArr(arrA, arrB, rng, wanted) {
+  
+  let arr = [];
+  let arrTwoOrMore =[]
+  let arrRanged = []
+  let nums = {}
+  
+  arrA.forEach(n => {
+    arrB.forEach(n2 => {
+      if(n == n2) arr.push(n)
+    })
+  })
+  arr.forEach(n => {
+    let num = n.toString()
+     nums[`${num}`] === undefined ?  nums[`${num}`] = 0 : nums[`${num}`] += 1 
+  } )
+  for( let num in nums) {
+    if(nums[num] >= 2) arrTwoOrMore.push(num)
+  }
+  arrTwoOrMore.forEach((n) => {
+    n = +n
+    if (n >= rng[0] && n <= rng[1]) arrRanged.push(n)
+  })
+  
+  let finalArray = wanted == 'even' ? arrRanged.filter(n => n % 2 == 0) : arrRanged.filter(n => n % 2 !== 0)
+  console.log(finalArray)
+  
+}
+
+let n = -7
+console.log(n, n.toString())
+n = n.toString()
+console.log (n, +n)
