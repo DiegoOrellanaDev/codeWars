7fcdf3e33ec4289a0c99b3f5ff4405fd7427dc18
Diego <iroh.doragon@gmail.com>
2025-04-25 15:55:48 -0600

    daily

--- ANeedleInTheHaystack.js
+++ ANeedleInTheHaystack.js
@@ -1,4 +1,5 @@
 //https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
 function findNeedle(haystack) {
     // your code here
     return `found the needle at position ${haystack.indexOf('needle') }`  
